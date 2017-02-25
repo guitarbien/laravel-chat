ea3dbea988410ba2ac5d9a2c65f8a2d8f7c2bbe6
guitarbien <guitarbien@gmail.com>
2017-02-25 16:04:38 +0800

    加入首頁，已可在本機註冊帳號後瀏覽

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -10,4 +10,9 @@ class ChatsController extends Controller
     {
         $this->middleware('auth');
     }
+
+    public function index()
+    {
+        return view('chat');
+    }
 }
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Chats</div>
+
+                <div class="panel-body">
+                    <!-- <chat-messages :messages="messages"></chat-messages> -->
+                    訊息顯示在這
+                </div>
+                <div class="panel-footer">
+                    訊息顯示在送出
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
